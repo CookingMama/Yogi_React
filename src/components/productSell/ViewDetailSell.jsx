5ad887ab61a3eac17f45399f9134c2fddf015204
youruchul <youruchul@naver.com>
2023-03-13 10:28:48 +0900

    solded product

--- src/components/productSell/ViewDetailSell.jsx
+++ src/components/productSell/ViewDetailSell.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import {
@@ -10,13 +10,27 @@ import { getProduct } from "../store/productSell/productSellSlice";
 const ViewDetailSell = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { data } = useSelector((state) => state.detailProduct);
+  const reduxData = useSelector((state) => state.detailProduct.data);
+  const [data, setData] = useState({
+    title: "",
+    url: "",
+    sellerNickName: "",
+    category: "",
+    price: 0,
+  });
   //   const location = useLocation();
   const param = useParams();
   console.log(param);
   useEffect(() => {
     dispatch(getDetailProduct(param.id));
   }, []);
+
+  useEffect(() => {
+    if (reduxData.id == param.id) {
+      setData(reduxData);
+    }
+  }, [reduxData]);
+
   const buyProductSell = () => {
     dispatch(putDetailProduct(param.id));
     navigate("/productsell");
@@ -29,25 +43,28 @@ const ViewDetailSell = () => {
         </h2>
 
         <div className="mt-6 flex justify-center">
-          {console.log(data)}
-          <div className="group relative w-1/3">
-            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
-              <img
-                src={data.url}
-                alt={data.url}
-                className="h-full w-full object-fill"
-              />
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <p className="mt-1 text-sm text-gray-500">
-                  {data.sellerNickName}
-                </p>
-                <p className="mt-1 text-sm text-gray-500">{data.category}</p>
+          {data && (
+            <div className="group relative w-1/3">
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                <img
+                  src={data.url}
+                  alt={data.url}
+                  className="h-full w-full object-fill"
+                />
               </div>
-              <p className="text-sm font-medium text-gray-900">{data.price}</p>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {data.sellerNickName}
+                  </p>
+                  <p className="mt-1 text-sm text-gray-500">{data.category}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {data.price}
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
       <div className="flex justify-end mr-5">
